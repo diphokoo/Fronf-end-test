3b435bfa7508a160c3b025247848f9514dd589d1
diphokoo <diphokoo@outlook.com>
2022-07-18 23:47:08 +0200

    NavBar PC styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,7 +13,7 @@
                     <li><a href="#!">Item 2</a></li>
                     <li><a href="#!">Item 3</a></li>
                     <li><a href="#!">Item 4</a></li>
-                    <li><a type="button">Button</a></li>
+                    <li><a type="button" href="#!" class="btn">Button</a></li>
                 </ul>
             </nav>
         </div>
--- src/main.js
+++ src/main.js
@@ -2,3 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+;(function($) {
+    $(function() {
+      $('nav ul li > a:not(:only-child)').click(function(e) {
+        $(this)
+          .siblings('.nav-dropdown')
+          .slideToggle()
+        $('.nav-dropdown')
+          .not($(this).siblings())
+          .hide()
+        e.stopPropagation()
+      })
+      $('html').click(function() {
+        $('.nav-dropdown').hide()
+      })
+      // Toggle open and close nav styles on click
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active')
+      })
+    })
+})(jQuery)
