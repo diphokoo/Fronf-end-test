120b9935004c47e9dad859213f7c2721fa67dbc2
diphokoo <diphokoo@outlook.com>
2022-07-18 22:35:12 +0200

    Exigent logo styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <section class="navigation"> 
         <div class="nav-container">
             <div class="company-logo">
-                <h2>logo</h2>
+                <h2>Exigent <span class="logo">Logo</span></h2>
             </div>
             <nav>
                 <div class="nav-mobile">
