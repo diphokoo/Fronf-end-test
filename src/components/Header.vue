b88070b0bec7a6250bc8744d22757090d46d1bb8
diphokoo <diphokoo@outlook.com>
2022-07-19 17:49:35 +0200

    adding text to the image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,10 @@
     <header>
         <div class="container">
             <img class="headerImg" src="./assets/header.png" />
+            <div class="centered">
+                <h1>{{ header }}</h1>
+                <p class="headerText"> {{ text }} </p>
+            </div>
         </div>
     </header>
 </template>
@@ -11,7 +15,7 @@ export default {
     data () {
         return {
             header: 'Main heading here',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris',
             buttonOne: 'Button One',
             buttonTwo: 'Button Two',
             btnText: 'Lorem Ipsum Dolor Sit',
