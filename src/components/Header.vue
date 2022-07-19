6bd000d2f51a0f620b38a9dfc2202750f64a00c2
diphokoo <diphokoo@outlook.com>
2022-07-19 16:03:45 +0200

    adding data to the file

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <div class="container">
+        
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            header: 'Main heading here',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra. Amet risus nullam eget felis eget nunc lobortis. Purus in massa tempor nec. Porta nibh venenatis',
+            buttonOne: 'Button One',
+            buttonTwo: 'Button Two',
+            btnText: 'Lorem Ipsum Dolor Sit',
+            pictire: '/src/assets/header.png',
+        }
+    }
+}
+</script>
